314f286accb306c11f3d66f7175f5b48fa21518a
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2023-09-06 14:17:15 -0400

    Update ZoneGenerator.java

--- src/main/java/net/pldyn/bluemapzones/ZoneGenerator.java
+++ src/main/java/net/pldyn/bluemapzones/ZoneGenerator.java
@@ -197,17 +197,32 @@ public class ZoneGenerator {
     }
 
     private boolean checkWest(Vector2d chunkMax, Vector2d chunkMin, HashMap<Vector2d,
-            ZonedChunk> knownChunks) {
+            ZonedChunk> knownChunks, Vector2d testID) {
+
+        int boundaryIntersections = 0;
+
+        boolean boundarySegment = false;
+
+        Vector2d adjChunkId = new Vector2d(testID.getFloorX(), testID.getFloorY() - 1);
+
+        if (knownChunks.containsKey(adjChunkId)) {
+            ZonedChunk adjChunk = knownChunks.get(adjChunkId);
+            if (adjChunk.isBoundary() && !boundarySegment) {
+                boundarySegment = true;
+                boundaryIntersections++;
+            }
+        }
+
         return false;
     }
 
     private boolean checkSouth(Vector2d chunkMax, Vector2d chunkMin, HashMap<Vector2d,
-            ZonedChunk> knownChunks) {
+            ZonedChunk> knownChunks, Vector2d testID) {
         return false;
     }
 
     private boolean checkEast(Vector2d chunkMax, Vector2d chunkMin, HashMap<Vector2d,
-            ZonedChunk> knownChunks) {
+            ZonedChunk> knownChunks, Vector2d testID) {
         return false;
     }
 
