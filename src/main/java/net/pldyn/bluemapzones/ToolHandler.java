02739b0464c520d2b0c6ef0c98251f70c61981e3
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-08-09 18:58:53 -0400

    Build: Syntax and Build Configs

--- src/main/java/net/pldyn/bluemapzones/ToolHandler.java
+++ src/main/java/net/pldyn/bluemapzones/ToolHandler.java
@@ -16,35 +16,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ToolHandler implements Listener {
-    private ArrayList<ZonedShape> zonedShapes = new ArrayList<>();
-    public ToolHandler(ArrayList<ZonedShape> zonedShapes) {
-        this.zonedShapes = zonedShapes;
-    }
-    @EventHandler
-    public void onPlayerUseTool(PlayerInteractEvent e) {
-        Player pc = e.getPlayer();
-        Material handItem = pc.getInventory().getItemInMainHand().getType();
-        Action completedAction = e.getAction();
+  private ArrayList<ZonedShape> zonedShapes = new ArrayList<>();
+  public ToolHandler(ArrayList<ZonedShape> zonedShapes) {
+    this.zonedShapes = zonedShapes;
+  }
+  @EventHandler
+  public void onPlayerUseTool(PlayerInteractEvent e) {
+    Player pc = e.getPlayer();
+    Material handItem = pc.getInventory().getItemInMainHand().getType();
+    Action completedAction = e.getAction();
 
-        if (completedAction == Action.RIGHT_CLICK_BLOCK) {
-            if (handItem == Material.COMPASS) {
-                Block interactedBlock = e.getClickedBlock();
-                assert interactedBlock != null;
-                Location blockLocation = interactedBlock.getLocation();
-                Vector2d chunkId = new Vector2d(Math.floorDiv(blockLocation.getBlockX(), 16),
-                        Math.floorDiv(blockLocation.getBlockZ(), 16));
-                ZonedChunk targetChunk = null;
+    if (completedAction == Action.RIGHT_CLICK_BLOCK) {
+      if (handItem == Material.COMPASS) {
+        Block interactedBlock = e.getClickedBlock();
+        assert interactedBlock != null;
+        Location blockLocation = interactedBlock.getLocation();
+        Vector2d chunkId = new Vector2d(Math.floorDiv(blockLocation.getBlockX(), 16),
+            Math.floorDiv(blockLocation.getBlockZ(), 16));
+        ZonedChunk targetChunk = null;
 
-                for (ZonedShape shape : zonedShapes) {
-                    HashMap<Vector2d, ZonedChunk> ownedChunks = shape.getOwnedChunks();
-                    if (ownedChunks.containsKey(chunkId)) targetChunk = ownedChunks.get(chunkId);
-                }
-
-                String zoneName = targetChunk != null ? targetChunk.getName() : "Wilderness";
-
-                pc.sendMessage("Currently inside chunk (" + chunkId.getX() + ", "
-                        + chunkId.getY() + ") - " + zoneName);
-            }
+        for (ZonedShape shape : zonedShapes) {
+          HashMap<Vector2d, ZonedChunk> ownedChunks = shape.getOwnedChunks();
+          if (ownedChunks.containsKey(chunkId)) targetChunk = ownedChunks.get(chunkId);
         }
+
+        String zoneName = targetChunk != null ? targetChunk.getName() : "Wilderness";
+
+        pc.sendMessage("Currently inside chunk (" + chunkId.getX() + ", "
+            + chunkId.getY() + ") - " + zoneName);
+      }
     }
+  }
 }
