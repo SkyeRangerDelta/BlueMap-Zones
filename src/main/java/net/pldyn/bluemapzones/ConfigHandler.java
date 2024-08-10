de28e40af7148dbbfcd8d84baf88ddebedf709c4
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-08-09 23:04:35 -0400

    New: Added exclusions config handling

--- src/main/java/net/pldyn/bluemapzones/ConfigHandler.java
+++ src/main/java/net/pldyn/bluemapzones/ConfigHandler.java
@@ -6,20 +6,42 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class ConfigHandler {
   private static final Logger Log = Logger.getLogger("BM Zones");
+
   private static File confFile;
   private static FileConfiguration pluginConfFile;
 
+  private static File noticeExclusionsConf;
+  private static FileConfiguration noticeExclusionsConfFile;
+
+  private static final List<File> fileConfigurations = new ArrayList<>();
+
+  /**
+   * @method confInit - Initialize the configuration files for the plugin.
+   */
   public static void confInit() {
+    // Create/Load configs
     filesInit();
+
+    // Set defaults for plugin settings
     pluginConfFile.options().copyDefaults(true);
     savePluginConfFile();
+
+    // Set defaults for notice UUIDs
+    noticeExclusionsConfFile.options().copyDefaults(true);
+    saveNoticeExclusionsConf();
   }
 
+  /**
+   * @method filesInit - Initialize the configuration files for the plugin.
+   */
   private static void filesInit() {
+    // Plugin configuration file
     confFile = new File(
       Bukkit.getPluginManager()
         .getPlugin("BlueMap-Zones")
@@ -34,19 +56,61 @@ public class ConfigHandler {
       }
     }
 
-    pluginConfFile = YamlConfiguration.loadConfiguration(confFile);
+    fileConfigurations.add(confFile);
+
+    // Notice Exclusions Configuration File
+    noticeExclusionsConf = new File(
+      Bukkit.getPluginManager()
+        .getPlugin("BlueMap-Zones")
+        .getDataFolder(), "BMZ-NoticeExclusions.yml");
+
+    if (!noticeExclusionsConf.exists()) {
+      try {
+        noticeExclusionsConf.createNewFile();
+      }
+      catch (IOException fileCreationErr) {
+        Log.warning("Unable to create a notice exclusions file on the server!\n" + fileCreationErr.getMessage());
+      }
+    }
+
+    fileConfigurations.add(noticeExclusionsConf);
+
+    loadConfig();
   }
 
+  /**
+   * @method loadConfig - Load the configuration files for the plugin.
+   */
+  private static void loadConfig() {
+    for (File f : fileConfigurations) {
+      if (f.getName().contains("BMZ-Config")) {
+        pluginConfFile = YamlConfiguration.loadConfiguration(f);
+      }
+      else if (f.getName().contains("BMZ-NoticeExclusions")) {
+        noticeExclusionsConfFile = YamlConfiguration.loadConfiguration(f);
+      }
+    }
+  }
+
+  /**
+   * @method createDefaults - Create default values for the plugin configuration.
+   */
   public static void createDefaults() {
     pluginConfFile.addDefault("Maps.name", "world");
     pluginConfFile.addDefault("Maps.marker-set", "myMarkers");
     savePluginConfFile();
   }
 
+  /**
+   * @method getPluginConfFile - Get the plugin configuration file.
+   */
   public static FileConfiguration getPluginConfFile() {
     return pluginConfFile;
   }
 
+  /**
+   * @method savePluginConfFile - Save the plugin configuration file.
+   */
   public static void savePluginConfFile() {
     try {
       pluginConfFile.save(confFile);
@@ -56,7 +120,22 @@ public class ConfigHandler {
     }
   }
 
+  /**
+   * @method saveNoticeExclusionsConf - Save the notice exclusions configuration file.
+   */
+  public static void saveNoticeExclusionsConf() {
+    try {
+      noticeExclusionsConfFile.save(noticeExclusionsConf);
+    }
+    catch (IOException fileSaveErr) {
+      Log.warning("Unable to save the notice exclusions configuration!");
+    }
+  }
+
+  /**
+   * @method reloadPluginConfgFile - Reload the plugin configuration files.
+   */
   public static void reloadPluginConfgFile() {
-    pluginConfFile = YamlConfiguration.loadConfiguration(confFile);
+    loadConfig();
   }
 }
